2269fa0b63f84ebc07680d64791d541955cfd11d
luowen <bigpao.luo@gmail.com>
2018-07-29 10:14:54 +0800

    optimize run.sh

--- run.sh
+++ run.sh
@@ -11,16 +11,16 @@ getpid() {
 }
 
 start() {
-    nohup python hot2dog.py > /dev/null &
+    nohup python hot2dog.py 2>&1 > ./error.log &
     printf "hot2dog.py startup: %s\n" `getpid`
 }
 
 stop() {
     pid=`getpid`
-    if [ !-z $pid ]
-    then
+    if [ -n "$pid" ]; then
         $(kill -9 $pid)
     fi
+    printf "hot2dog.py stop ok  pid[%s]\n" $pid
 }
 
 status() {
@@ -44,6 +44,9 @@ case "$1" in
     status)
         status
     ;;
+    restart)
+        stop
+        start
     *)
     echo $"Usage: $0 {start | stop | status}"
     exit 1
